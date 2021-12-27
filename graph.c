72b472000b85a53f8ab02f79013b0e766ec57605
Dana Zorohov <93203695+danaZo@users.noreply.github.com>
2021-12-27 11:59:11 +0200

    Add files via upload

---
+++ graph.c
@@ -0,0 +1,13 @@
+/* CLASS GRAPH:
+   In this class we create a graph, using linked list that holds the nodes of the graph.
+   Each node holds the information about the edges that goes from the node.*/
+
+#include "graph.h"
+#include "nodes.h"
+
+typedef struct graph
+{
+    pnode nodes; // node will be implemented as linked list
+
+};
+
