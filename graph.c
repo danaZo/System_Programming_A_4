38e666ba502e9a9a9c395f7a86eacfa0d01fab9d
danaZo <93203695+danaZo@users.noreply.github.com>
2021-12-28 15:43:25 +0200

    testing

--- graph.c
+++ graph.c
@@ -4,10 +4,61 @@
 
 #include "graph.h"
 #include "nodes.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void insert_node_cmd(pnode *head)
 {
+   int id;
+   printf("enter id of key: ");
+   scanf("%d",&id);
+   pnode np = create_node(id);
+   if(head == NULL){
+      *head = np;
+   } 
+   else{
+      pnode* holder = head;
+      while(&(*holder)->next != NULL){
+         (*holder) = &(*holder)->next;
+      }
+      
+   }
+}
 
+void build_graph_cmd(pnode *head)
+{
+   int numOfNodes;                // will hold the number of the nodes in the graph
+   int srcNode, destNode, weight;
+
+   /* getting  the number of nodes */
+   scanf("%d", &numOfNodes);
+   printf("%d\n", numOfNodes); /*DELETE LATER*/
+
+   int n_counter = 0;
+   int counter =0;
+   int isFirst =1;
+
+
+   while(n_counter != numOfNodes)
+   {
+      int currentNum = 0;
+      while(scanf("%d"),&currentNum){
+         if(isFirst){
+            pnode np = create_node(currentNum);
+            isFirst = 0;
+            *head = np;
+         }
+         else{
+            if(counter%2 == 0){
+               destNode = currentNum;
+               counter++;
+            }
+            else{
+
+            }
+         }
+      }
+   }
 }
 
 
